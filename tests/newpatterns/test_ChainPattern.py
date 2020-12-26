4ba2d5e25c8ddbab8a25b1a3fe3cfc954345684b
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-12-26 16:37:07 -0500

    Refactoring pattern tests

--- tests/newpatterns/test_ChainPattern.py
+++ tests/newpatterns/test_ChainPattern.py
@@ -6,65 +6,53 @@ from supriya.newpatterns import (
     NoteEvent,
     SequencePattern,
 )
+from supriya.newpatterns.testutils import MockUUID as M
+from supriya.newpatterns.testutils import run_event_pattern_test
 
 
 @pytest.mark.parametrize(
-    "input_a, input_b1, input_b2, input_c, expected, is_infinite",
+    "stop_at, input_a, input_b1, input_b2, input_c, expected, is_infinite",
     [
         (
+            None,
             SequencePattern([1, 2, 3]),
             SequencePattern([4, 5]),
             SequencePattern([7, 8, 9]),
             SequencePattern([10, 11]),
-            [{"a": 1, "b": 7, "c": 10}, {"a": 2, "b": 8, "c": 11}],
+            [NoteEvent(M("A"), a=1, b=7, c=10), NoteEvent(M("B"), a=2, b=8, c=11)],
             False,
         ),
         (
+            None,
             SequencePattern([1, 2, 3], None),
             SequencePattern([4, 5], None),
             SequencePattern([7, 8, 9]),
             SequencePattern([10, 11]),
-            [{"a": 1, "b": 7, "c": 10}, {"a": 2, "b": 8, "c": 11}],
+            [NoteEvent(M("A"), a=1, b=7, c=10), NoteEvent(M("B"), a=2, b=8, c=11)],
             False,
         ),
         (
+            None,
             SequencePattern([1, 2, 3], None),
             SequencePattern([4, 5], None),
             SequencePattern([7, 8, 9], None),
             SequencePattern([10, 11], None),
             [
-                {"a": 1, "b": 7, "c": 10},
-                {"a": 2, "b": 8, "c": 11},
-                {"a": 3, "b": 9, "c": 10},
-                {"a": 1, "b": 7, "c": 11},
-                {"a": 2, "b": 8, "c": 10},
-                {"a": 3, "b": 9, "c": 11},
+                NoteEvent(M("A"), a=1, b=7, c=10),
+                NoteEvent(M("B"), a=2, b=8, c=11),
+                NoteEvent(M("C"), a=3, b=9, c=10),
+                NoteEvent(M("D"), a=1, b=7, c=11),
+                NoteEvent(M("E"), a=2, b=8, c=10),
+                NoteEvent(M("F"), a=3, b=9, c=11),
             ],
             True,
         ),
     ],
 )
-def test(input_a, input_b1, input_b2, input_c, expected, is_infinite):
+def test(stop_at, input_a, input_b1, input_b2, input_c, expected, is_infinite):
     pattern = ChainPattern(
         EventPattern(a=input_a, b=input_b1),
         EventPattern(b=input_b2),
         EventPattern(c=input_c),
     )
-    assert pattern.is_infinite == is_infinite
-    iterator = iter(pattern)
-    actual = []
-    ceased = True
-    for iteration in range(1000):
-        try:
-            event = next(iterator)
-            assert isinstance(event, NoteEvent)
-            actual.append(event.kwargs)
-        except StopIteration:
-            break
-    else:
-        ceased = False
-    if is_infinite:
-        assert not ceased
-        assert actual[: len(expected)] == expected
-    else:
-        assert actual == expected
+    run_event_pattern_test(pattern, expected, is_infinite, stop_at)
