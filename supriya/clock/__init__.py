3aa965e072a3bf39736ee2ddd482fbd749cc10e7
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-12-28 11:37:38 -0500

    Reformat

--- supriya/clock/__init__.py
+++ supriya/clock/__init__.py
@@ -2,6 +2,4 @@ from .asynchronous import AsyncTempoClock
 from .ephemera import ClockContext, Moment, TimeUnit
 from .threaded import TempoClock
 
-__all__ = [
-    "AsyncTempoClock", "ClockContext", "Moment", "TempoClock", "TimeUnit"
-]
+__all__ = ["AsyncTempoClock", "ClockContext", "Moment", "TempoClock", "TimeUnit"]
